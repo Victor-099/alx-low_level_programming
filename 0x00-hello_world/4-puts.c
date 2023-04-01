477a2cf93aceb3175d48c3edbf447d2e24f58e6b
Victor-099 <ndolovicky4@gmail.com>
2023-04-01 13:26:14 -0700

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+ * main -a program that prints a line with puts function
+ *Return: 0 (success)
+ *
+ */
+int main(void)
+{
+puts("\nProgramming is like building a multilingual puzzle");
+return (0);
+}
+
